473ef8d3d7931f1646bd2350be880b7284fc024c
Kelly Zhou <kellyzhou@Rongmaos-MacBook-Pro-2.local>
2022-08-06 00:01:58 -0500

    added design to splash screen and sign up screen

---
+++ app/src/main/java/com/example/shareme/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.shareme;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //timer for splash screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
